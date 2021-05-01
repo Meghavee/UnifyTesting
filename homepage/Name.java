fb8f8f18e6d498e7e79a7881cb5e2b107f294d63
Meghavee <Meghas1010@gmail.com>
2021-05-02 00:57:55 +0100

    New files

---
+++ homepage/Name.java
@@ -0,0 +1,11 @@
+package com.nopecommerce.homepage;
+
+public class Name
+{
+    public static void main(String []args)
+
+    {
+        System.out.print("Meghavee ");
+        System.out.println("Valand");
+    }
+}
---
+++ homepage/OddEven.java
@@ -0,0 +1,16 @@
+package com.nopecommerce.homepage;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int odd = 1;
+        int even = 2;
+        if (odd % 2 == 1) {
+            System.out.print("Number is odd");
+        } else if (even % 1 == 2) {
+             System.out.println("Number is even");
+        }
+
+    }
+}
+
+
